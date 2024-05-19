00fc17d78df8bf5bb71f6368581a94dc861b74aa
junhyeongchae <jchae7184@gmail.com>
2024-05-19 18:43:16 +0900

    카테고리별 수입 지출 내역 stats

--- schema/categories.ts
+++ schema/categories.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const CreateCategorySchema = z.object({
-  name: z.string().min(3).max(20),
+  name: z.string().min(2).max(20),
   icon: z.string().max(20),
   type: z.enum(["income", "expense"]),
 });
