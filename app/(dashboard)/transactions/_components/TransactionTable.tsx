600e79485839c82c96134288ee96cd6f4ba91af3
junhyeongchae <jchae7184@gmail.com>
2024-05-19 21:49:44 +0900

    내역 페이지 테이블 설정(검색, 필터, 페이지네이션 추가)

---
+++ app/(dashboard)/transactions/_components/TransactionTable.tsx
@@ -0,0 +1,267 @@
+"use client";
+import { GetTransactionHistoryResponseType } from "@/app/api/transactions-history/route";
+import { DateToUTCDate } from "@/lib/helpers";
+import { useQuery } from "@tanstack/react-query";
+import React, { useMemo, useState } from "react";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+} from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
+import { DownloadIcon } from "lucide-react";
+import SkeletonWrapper from "@/components/SkeletonWrapper";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { DataTableColumnHeader } from "@/components/datatable/ColumnHeader";
+import { cn } from "@/lib/utils";
+import { DataTableFacetedFilter } from "@/components/datatable/FacedFilters";
+import { DataTableViewOptions } from "@/components/datatable/ColumnToggle";
+
+interface Props {
+  from: Date;
+  to: Date;
+}
+
+const emptyData: any[] = [];
+
+type TransactionHistoryRow = GetTransactionHistoryResponseType[0];
+
+const columns: ColumnDef<TransactionHistoryRow>[] = [
+  {
+    accessorKey: "category",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="카테고리" />
+    ),
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id));
+    },
+    cell: ({ row }) => (
+      <div className="flex gap-2 capitalize">
+        {row.original.categoryIcon}
+        <div className="capitalize">{row.original.category}</div>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "description",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="내용" />
+    ),
+    cell: ({ row }) => (
+      <div className="capitalize">{row.original.description}</div>
+    ),
+  },
+  {
+    accessorKey: "date",
+    header: "날짜",
+    cell: ({ row }) => {
+      const date = new Date(row.original.date);
+      const formattedDate = date.toLocaleDateString("default", {
+        timeZone: "UTC",
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      });
+      return <div className="text-muted-foreground">{formattedDate}</div>;
+    },
+  },
+  {
+    accessorKey: "type",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="종류" />
+    ),
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id));
+    },
+    cell: ({ row }) => (
+      <div
+        className={cn(
+          "capitalize rounded-lg text-center p-2",
+          row.original.type === "income" &&
+            "bg-emerald-400/10 text-emerald-500",
+          row.original.type === "expense" && "bg-red-400/10 text-red-500",
+        )}
+      >
+        {row.original.type === "income" ? "수입" : "지출"}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "amount",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="금액" />
+    ),
+    cell: ({ row }) => (
+      <p className="text-md rounded-lg bg-gray-400/5 p-2 text-left font-medium">
+        {row.original.formattedAmount}
+      </p>
+    ),
+  },
+  // {
+  //   id: "actions",
+  //   enableHiding: false,
+  //   cell: ({ row }) => <RowActions transaction={row.original} />,
+  // },
+];
+
+const TransactionTable = ({ from, to }: Props) => {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+
+  const history = useQuery<GetTransactionHistoryResponseType>({
+    queryKey: ["transactions", "history", from, to],
+    queryFn: () =>
+      fetch(
+        `/api/transactions-history?from=${DateToUTCDate(
+          from,
+        )}&to=${DateToUTCDate(to)}`,
+      ).then((res) => res.json()),
+  });
+
+  const table = useReactTable({
+    data: history.data || emptyData,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    // CHECK: 페이지네이션 설정
+    // initialState: {
+    //   pagination: {
+    //     pageSize: 2,
+    //   },
+    // },
+    state: {
+      sorting,
+      columnFilters,
+    },
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  });
+
+  const categoriesOptions = useMemo(() => {
+    const categoriesMap = new Map();
+    history.data?.forEach((transaction) => {
+      categoriesMap.set(transaction.category, {
+        value: transaction.category,
+        label: `${transaction.categoryIcon} ${transaction.category}`,
+      });
+    });
+    const uniqueCategories = new Set(categoriesMap.values());
+    return Array.from(uniqueCategories);
+  }, [history.data]);
+
+  return (
+    <div className="w-full">
+      <div className="flex flex-wrap items-end justify-between gap-2 py-4">
+        <div className="flex">
+          {table.getColumn("category") && (
+            <DataTableFacetedFilter
+              title="카테고리"
+              column={table.getColumn("category")}
+              options={categoriesOptions}
+            />
+          )}
+          {table.getColumn("type") && (
+            <DataTableFacetedFilter
+              title="종류"
+              column={table.getColumn("type")}
+              options={[
+                { label: "수입", value: "income" },
+                { label: "지출", value: "expense" },
+              ]}
+            />
+          )}
+        </div>
+        <div className="flex flex-wrap gap-2">
+          <DataTableViewOptions table={table} />
+        </div>
+      </div>
+      <SkeletonWrapper isLoading={history.isFetching}>
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext(),
+                            )}
+                      </TableHead>
+                    );
+                  })}
+                </TableRow>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    결과가 없습니다.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+        <div className="flex items-center justify-end space-x-2 py-4">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            이전
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            다음
+          </Button>
+        </div>
+      </SkeletonWrapper>
+    </div>
+  );
+};
+
+export default TransactionTable;
